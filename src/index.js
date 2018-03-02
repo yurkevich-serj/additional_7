8e0912c53e2611348ed885924b45dec54d9e1cdd
yurkevich-serj <yurkevichserj@gmail.com>
2018-03-02 18:34:04 +0300

    Solved this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+function findSolutionForGranny(matrix){
+     let helpMatrix;
+     for (let i = 0; i < 9; i++) {
+         for (let j = 0; j < 9; j++) {
+              if (matrix[i][j] == 0) {
+                 for (let varient= 1; varient <= 9; varient++) {
+                     if (uniquenessLineColumn(matrix, i, j, varient) && uniquenessSquare(matrix, i, j, varient)) {
+                         matrix[i][j] = varient;
+                         helpMatrix = findSolutionForGranny(matrix);
+                         if (helpMatrix) {
+                             return matrix;
+                         }
+                         matrix[i][j] = 0;
+                     }
+                }
+                return false;
+             }
+         }
+     }
+     return true;
 }
+function uniquenessLineColumn(matrix, line, collumn, varient) {
+    for (let i = 0; i < 9; i++) {
+         if (i != line && matrix[i][collumn] == varient || i != collumn && matrix[line][i] == varient) {
+            return false;
+           } 
+        }
+    return true;
+    }
+function uniquenessSquare(matrix, line, collumn, varient) {
+     for (let i = 0; i < 3; i++) {
+         for (let j = 0; j < 3; j++) {
+             if (i != line && j != collumn && matrix[Math.floor((line / 3)) * 3 + i][Math.floor((collumn / 3)) * 3 + j] == varient) {
+                 return false;
+              }
+         }
+     }
+    return true
+    }
+return findSolutionForGranny(matrix);
+}
+
+
